c24f913c4856cd69563fd06000358a95cb76125a
Tasleemali <133391205+Tasleemali@users.noreply.github.com>
2025-02-13 15:24:00 +0530

    Update index.js

--- src/app/compo/header/index.js
+++ src/app/compo/header/index.js
@@ -1,43 +1,46 @@
 import { Button } from '@/components/ui/button'
 import { Facebook, Instagram, Linkedin, X } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
+import third from "@/components/assets/third.png"
+
 
 function Header() {
     return (
-        <div id='header' className='bg-gray-900 text-white'>
+        <div id='header' className='bg-gray-900 text-white mt-10 pt-20'>
 
-            <div className='mx-auto max-w-screen-xl  '>
-                 
-                <div className=' mx-10 px-10 py-10 flex flex-wrap justify-center items-center text-center md:text-start ite md:gap-10'>
+            <div className='  mx-auto max-w-screen-xl  '>
 
-               <div className='md:w-auto' >
-                <img className=' w-[250px] md:w-[300px] rounded-full text-right' src='https://thumbs.dreamstime.com/b/portrait-young-handsome-happy-man-wearing-glasses-casual-smart-blue-clothing-yellow-color-background-square-composition-200740125.jpg' alt='photo'/>
-               </div>
+                <div className=' mx-10 px-10 py-10 flex flex-wrap justify-center items-center text-center md:text-start gap-5 md:gap-10'>
 
-                <div className=' md:w-auto px-5 text-wrap'>
-                   <h1 className=' text-xl sm:text-2xl md:text-4xl'>Hi,It's <span className='text-red-500'>Tasleem</span> </h1>
-                   <h3 className='text-xl'>I'm a <span className='text-red-500'> Web Devloper</span></h3>
-                   <p className='  text-xm sm:text-sm md:text-lg md:w-[400px] text-wrap'>Passionate about creating user-centric, responsive websites. I specialize in front-end and back-end development to bring your ideas to life. Let’s build something amazing together!
+                    <div className='md:w-auto' >
+                        <Image className=' bg-red-500 w-[250px] md:w-[300px] rounded-full text-right' src={third} alt='photo' />
+                    </div>
 
-                  </p>
-                  
-                  <div>
-                    <ul className='flex md:justify-start justify-center items-center gap-10 py-5'>
-                        <li className=" py-1 px-1 border-2 border-blue-500 text-blue-400 rounded-lg hover:shadow-custom-shadow transition-shadow duration-300"><Facebook className='w-4 h-4'/></li>
-                        <li className="  py-1 px-1 border-2 border-white text-white rounded-lg hover:shadow-customs-shadow transition-shadow duration-300"><X className='w-4 h-4'/></li>
-                        <li className=" py-1 px-1 border-2 border-rose-600 text-rose-600 rounded-lg hover:shadow-insta-shadow transition-shadow duration-300"><Instagram className='w-4 h-4' /></li>
-                        <li className=" py-1 px-1 border-2 border-blue-500 text-blue-400 rounded-lg hover:shadow-custom-shadow transition-shadow duration-300"><Linkedin className='w-4 h-4'/></li>
-                   </ul>
+                    <div className=' md:w-auto px-5 text-wrap text-center lg:text-start'>
+                        <h1 className=' text-xl sm:text-2xl md:text-4xl'>Hi,It's <span className='text-red-500'>Tasleem</span> </h1>
+                        <h3 className='text-xl'>I'm a <span className='text-red-500'> Web Devloper</span></h3>
+                        <p className='  text-xm sm:text-sm md:text-lg md:w-[400px] text-wrap'>Passionate about creating user-centric, responsive websites. I specialize in front-end and back-end development to bring your ideas to life. Let’s build something amazing together!
 
-                   <Button className='border-2 border-red-500 rounded-2xl  text-white hover:border-blue-200 hover:bg-red-500  '>More About me</Button>
-                  </div>
+                        </p>
+
+                        <div>
+                            <ul className='flex md:justify-start justify-center items-center gap-10 py-5'>
+                                <li className=" py-1 px-1 border-2 border-blue-500 text-blue-400 rounded-lg hover:shadow-custom-shadow transition-shadow duration-300"><Facebook className='w-4 h-4' /></li>
+                                <li className="  py-1 px-1 border-2 border-white text-white rounded-lg hover:shadow-customs-shadow transition-shadow duration-300"><X className='w-4 h-4' /></li>
+                                <li className=" py-1 px-1 border-2 border-rose-600 text-rose-600 rounded-lg hover:shadow-insta-shadow transition-shadow duration-300"><Instagram className='w-4 h-4' /></li>
+                                <li className=" py-1 px-1 border-2 border-blue-500 text-blue-400 rounded-lg hover:shadow-custom-shadow transition-shadow duration-300"><Linkedin className='w-4 h-4' /></li>
+                            </ul>
+
+                            <a href='#contect'>  <Button className='border-2 border-red-500 rounded-2xl  text-white hover:border-blue-200 hover:bg-red-500  '>Hire me</Button></a>
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
+
                 </div>
-
-                <div>
-
-                </div>
-
-            </div>
 
 
             </div>
@@ -46,3 +49,4 @@ function Header() {
 }
 
 export default Header
+
