f1429d3654e6168e5119f5b03c40fbbc624ff0bd
Tasleemali <133391205+Tasleemali@users.noreply.github.com>
2025-02-13 15:24:53 +0530

    Update index.js

--- src/app/compo/navbar/index.js
+++ src/app/compo/navbar/index.js
@@ -1,57 +1,47 @@
 
 "use client"
-import { Button } from '@/components/ui/button'
-import { Facebook, Instagram, Linkedin, X } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
-import third from "@/components/assets/third.png"
+import { Menu } from 'lucide-react'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
-
-function Header() {
+function Navbar() {
+F
+    const [activeMenu ,setActiveMenu] = useState('home')
+    const [menu ,setmenu] = useState(false)
     return (
-        <div id='header' className='bg-gray-900 text-white mt-10 pt-20'>
+        <div className='bg-gray-900 fixed  top-0 left-0 right-0  text-white '>
+            <div className= ' mx-auto max-w-screen-xl px-5 sm:px-8 md-px-10 py-10'>
 
-            <div className='  mx-auto max-w-screen-xl  '>
+                <div className=' w-full flex justify-between items-center'>
+                    <div className=''>
+                        <span className='text-3xl font-bold  self-center whitespace-nowrap text-red-500 shadow-lg'  >Tasleem.</span>
+                    </div> 
 
-                <div className=' mx-10 px-10 py-10 flex flex-wrap justify-center items-center text-center md:text-start gap-5 md:gap-10'>
-
-                    <div className='md:w-auto' >
-                        <Image className=' bg-red-500 w-[250px] md:w-[300px] rounded-full text-right' src={third} alt='photo' />
+                    <div className={`${menu?'fixed':'hidden'}  top-20 left-0 right-0 z-20  md:flex `}>
+{/* 
+                        <span onClick={()=>setmenu(!menu)} className='md:hidden'>Back</span> */}
+                    <ul className= '  flex  z-10 flex-col md:flex-row justify-between items-center gap-10'>
+                   <a className='' href='#header'>    <li value={"home"}  onClick={()=> {setActiveMenu('home'),setmenu(false)}} className={`${activeMenu === 'home' ?"border-b border-red-500 text-red-500 ":''}`} >Home</li></a> 
+                   <a href='#about'>   <li value={'about'} onClick={()=> {setActiveMenu('about'),setmenu(false)}}className={`${activeMenu === 'about' ?" border-b border-red-500 text-red-500":''}`} >About</li></a> 
+                   <a href='#project'>    <li value={"project"} onClick={()=> {setActiveMenu('project'),setmenu(false)}} className={`${activeMenu === 'project' ?"border-b border-red-500 text-red-500  ":''}`}>project</li></a> 
+                  <a href='#contect'> <li value={"service"} onClick={()=> {setActiveMenu('service'),setmenu(false)}} className={`${activeMenu === 'service' ?"border-b border-red-500 text-red-500 ":''}`}>Contect</li></a>  
+                    </ul>
                     </div>
-
-                    <div className=' md:w-auto px-5 text-wrap text-center lg:text-start'>
-                        <h1 className=' text-xl sm:text-2xl md:text-4xl'>Hi,It's <span className='text-red-500'>Tasleem</span> </h1>
-                        <h3 className='text-xl'>I'm a <span className='text-red-500'> Web Devloper</span></h3>
-                        <p className='  text-xm sm:text-sm md:text-lg md:w-[400px] text-wrap'>Passionate about creating user-centric, responsive websites. I specialize in front-end and back-end development to bring your ideas to life. Let’s build something amazing together!
-
-                        </p>
-
-                        <div>
-                            <ul className='flex md:justify-start justify-center items-center gap-10 py-5'>
-                                <li className=" py-1 px-1 border-2 border-blue-500 text-blue-400 rounded-lg hover:shadow-custom-shadow transition-shadow duration-300"><Facebook className='w-4 h-4' /></li>
-                                <li className="  py-1 px-1 border-2 border-white text-white rounded-lg hover:shadow-customs-shadow transition-shadow duration-300"><X className='w-4 h-4' /></li>
-                                <li className=" py-1 px-1 border-2 border-rose-600 text-rose-600 rounded-lg hover:shadow-insta-shadow transition-shadow duration-300"><Instagram className='w-4 h-4' /></li>
-                                <li className=" py-1 px-1 border-2 border-blue-500 text-blue-400 rounded-lg hover:shadow-custom-shadow transition-shadow duration-300"><Linkedin className='w-4 h-4' /></li>
-                            </ul>
-
-                            <a href='#contect'>  <Button className='border-2 border-red-500 rounded-2xl  text-white hover:border-blue-200 hover:bg-red-500  '>Hire me</Button></a>
-                        </div>
-                    </div>
-
-                    <div>
-
-                    </div>
+                    <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="   inline-flex items-center p-2 text-sm text-white rounded-lg md:hidden hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+            onClick={()=>setmenu(!menu)}
+           
+          >
+            <Menu className='w-10 h-10'/>
+          </button>
 
                 </div>
-
-
             </div>
         </div>
-    )
-}
-
-export default Header
-
     )
 }
 
